ef52f52e558eeb508032e920677fc3bd4e2fa3e1
linzhiji <linzhiji@ishehui.com>
2025-04-02 16:32:30 +0800

    fix bug

---
+++ mongo.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+mkdir -p /data/work/mongo/config
+mkdir -p /data/work/mongo/data
+mkdir -p /data/work/mongo/logs
+touch /data/work/mongo/config/mongod.conf
+chmod 777 /data/work/mongo
+
